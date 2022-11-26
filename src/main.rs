fa4f1e6043a0064396df415dab5ca0d1d1a3d80b
adamwoolhether <adamwoolhether@gmail.com>
2022-11-26 12:44:09 +0800

    using iterator and optinos instead of looping over array and returning bool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self { // accept str, but store as String.
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn name() -> String{
     let mut your_name = String::new();
     stdin()
@@ -15,17 +32,17 @@ fn main() {
     let name = name();
     println!("Hello, {}", name); // use {:?} for debugging.
 
-    let guest_list = ["bert", "steve", "fred"];
-    let mut allow_them_in = false;
-    for visitor in &guest_list { // use & to borrow, not copy the data.
-        if visitor == &name {
-            allow_them_in = true;
-        }
+    let guest_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is expired."),
+        Visitor::new("fred", "Fred, you were invited?!"),
+    ];
+    let known_guest = guest_list
+        .iter() // using an iterator instead of looping and returning bool!
+        .find(|visitor| visitor.name == name); // find() will run a `closure`, returning an `Option`
+    match known_guest { // run a match statement on the `Option` returned by find()
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You aren't on the guest list, please leave.")
     }
 
-    if allow_them_in {
-        println!("Welcome to the treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
-    }
 }
